5d7fbbb4d1d2187902efd0bda8e4f7e75e61f77c
babiy <babiy6581@gmail.com>
2026-04-20 09:05:13 +0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Menu from "./components/menu/Menu";
 import About from './pages/About';
